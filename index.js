788ee41a6fe68c3b69cfbc543c65fec2f48e3fd2
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-20 12:14:41 -0600

    labwork completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.querySelector("p").textContent = "This is really cool!"
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
